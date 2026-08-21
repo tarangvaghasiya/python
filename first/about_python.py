6406c11a704a1ffe7fa6229f3d94d538168681dd
Tarang Vaghasiya <tarangvaghasiya.cg@gmail.com>
2026-08-21 17:57:37 +0530

    this is biging

---
+++ first/about_python.py
@@ -0,0 +1,4 @@
+print("Hello, World!")
+print("Welcome to Python programming.")
+print("Let's get started with some basic concepts.")
+print("Python is a versatile and powerful programming language.")
