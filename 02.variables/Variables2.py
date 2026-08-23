d6bb33f0da3186397140590e84be57a2d2b0a3a7
Tarang Vaghasiya <tarangvaghasiya.cg@gmail.com>
2026-08-23 21:12:43 +0530

    for variable

---
+++ 02.variables/Variables2.py
@@ -0,0 +1,8 @@
+student_name = "John Doe"
+student_age = 21
+student_city = "New York"
+student_branch = "Computer Science"
+print(student_name)
+print(student_age)  
+print(student_city)
+print(student_branch)
