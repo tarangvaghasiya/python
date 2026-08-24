9d85e04688ba64547c9d6e00dd1ac60757651d87
Tarang Vaghasiya <tarangvaghasiya.cg@gmail.com>
2026-08-24 11:09:11 +0530

    Create datatypes.py

---
+++ 03.Data_Types_and_Type_Casting/datatypes.py
@@ -0,0 +1,16 @@
+age=43
+name="tarang"
+marks=90.5
+passrue = True
+result=None
+print(age)
+print(name)
+print(marks)
+print(passrue)
+print(result)
+
+print(type(age))
+print(type(name))
+print(type(marks))
+print(type(passrue))
+print(type(result))
