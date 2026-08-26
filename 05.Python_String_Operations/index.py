3a0acf53146fa6c2ecfe03fb6a586c7ed3786223
Tarang Vaghasiya <tarangvaghasiya.cg@gmail.com>
2026-08-26 22:15:11 +0530

    grgergfer

---
+++ 05.Python_String_Operations/index.py
@@ -0,0 +1,21 @@
+print("taraansdc \n usdfhfyf")
+a="tarang"
+print(a[3])
+print("""ojgjghuthgtggg
+          regetgergerg
+           regergergr  ergerg
+           ergergegrger     tgrg""")
+print(a[-4])
+print(a[0:4])
+print(a[:3])
+print(a[2:])
+
+word = "python"
+
+copy = word[1:4]
+
+print(copy)
+
+
+print(word[::-1])
+print(len(word))
