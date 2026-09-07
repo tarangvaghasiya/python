85702342184c5dc387be75a0d58e39ca03eec4c6
Tarang Vaghasiya <tarangvaghasiya.cg@gmail.com>
2026-09-07 09:39:57 +0530

    assignment

---
+++ 08.conditional_statements/as.py
@@ -0,0 +1,139 @@
+#1
+
+a=10
+if a>10:
+    print("a is greater than 10")
+
+#2
+age=20
+if age>=18:
+    print("You are eligible to vote")
+
+#3
+b=int(input("Enter your number:"))
+if b > 0:
+    print("You have entered a positive number")
+
+#4
+c=int(input("Enter your marks:"))
+if c >= 40:
+    print("You have passed the exam")
+else:
+    print("You have failed the exam")
+
+#5
+d=int(input("Enter your number:"))
+if d == 0:
+    print("You have entered zero")
+
+#6
+e=int(input("Enter your number:"))
+if e > 0:
+    print("You have entered a positive number")
+else:
+    print("You have entered a negative number")
+
+#7
+age1=int(input("Enter your age:"))
+if age1 >= 18:
+    print("adult")
+else:
+    print("minor")
+
+#8
+a1=int(input("Enter a number: "))
+if a1 % 2 == 0:
+    print("your number is Even")
+else:
+    print("your number is Odd")
+
+#9
+marks1=int(input("Enter your marks: "))
+if marks1 >= 40:
+    print("You have passed the exam")
+else:
+    print("You have failed the exam")
+
+#11
+marks2=int(input("Enter your marks: "))
+if marks2 >= 90:
+    print("You have got A grade")   
+elif marks2 >=75:
+        print("You have got b grade")  
+elif marks2 >=60:
+        print("You have got c grade") 
+elif marks2 >=40:
+        print("You have got d grade")
+else:
+    print("you are fail in exam")  
+
+#12
+
+num=input("input your number")
+if num > 0:
+    print("your number is positive")
+elif num == 0:
+    print("your number is zero")
+else:
+    print("your number is negetive")
+
+#13
+
+num1=input("choose your number(1,2,3,4,5)")
+if num1 == 1:
+    print("you choose monday")
+elif num1 == 2:
+    print("you choose tuesday")
+elif num1 == 3:
+    print("you choose wedneaday")
+elif num1 == 4:
+    print("you choose thursday")
+elif num1 == 5:
+    print("you choose friay")
+
+#14
+
+marks3=input("enter your marks")
+
+if marks3 > 90:
+    print("excellent")
+elif marks3 > 75:
+    print("good")
+elif marks3 > 33:
+    print("pass")
+else:
+    print("fail")
+
+#15
+
+num2=input("input your number")
+if num2 == 1:
+   print("number is 1")
+elif num2 == 2:
+    print("number is 2")
+elif num2 == 3:
+    print("number is 3")
+else:
+    print("other")
+
+#16
+
+a2 = int(input("Enter the age: "))
+if a2 >= 18:
+    if a2 <= 60:
+        print("Between 18 and 60")
+
+
+#17
+
+marks4=input("your marks")
+if marks4 >= 40:
+    print("PASS")
+elif marks >= 75:
+    print("good")
+else:
+    print("failed")
+
+#18
+
+
--- 08.conditional_statements/pro.py
+++ 08.conditional_statements/pro.py
@@ -1,5 +1,29 @@
-a=(int(input("Enter a number: ")))
-if a % 2 == 0:
-    print("your number is Even")
+# a=(int(input("Enter a number: ")))
+# if a % 2 == 0:
+#     print("your number is Even")
+# else:
+#     print("your number is Odd")
+
+age=int(input("Enter your age: ").split()[0])
+if age >= 18:
+    print("You are eligible to watch this movie")
+    gender=str(input("Enter your gender: ").split()[0])
+    if gender == "male":
+        print("Your ticket price is 300-500")
+        class_input=int(input("Enter your class(1,2,3): "))
+        if class_input == 1:
+            print("Your ticket price is 500")
+        elif class_input == 2:
+            print("Your ticket price is 400")
+        elif class_input == 3:
+            print("Your ticket price is 300")    
+    elif gender == "female":
+        print("Your ticket price is 100-300")
+        if class_input == 1:
+            print("Your ticket price is 300")
+        elif class_input == 2:
+            print("Your ticket price is 200")
+        elif class_input == 3:
+            print("Your ticket price is 100") 
 else:
-    print("your number is Odd")
+    print("You are not eligible to watch this movie")
