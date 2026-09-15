4e90e64298b8e50462144cfadc69bcccf318ce54
Tarang Vaghasiya <tarangvaghasiya.cg@gmail.com>
2026-09-15 19:08:23 +0530

    saadad

--- 10.For_Loop/as1.py
+++ 10.For_Loop/as1.py
@@ -224,5 +224,5 @@
 
 
 #####
-#fvfg8dfdfsfdfgdfgghghghbcvbcbbdfdfsasas,,h,,f bnfgghghghnnhnmnmnfffgtgtgrfrfvcvggghghgggggttfrfrfrgfgf
+#fvfg8dfdfsfdfgdfgghghghbcvbcbbdfdfsasas,,h,,f bnfgghghghnnhnmnmnfffgtgtgrfrfvcvggghghgggggttfrfrfrgfgfdsdd
 #qwvbbbvbdfvddffdfdvdvffdsfdfgfghthtfffdeererffgfgfgfgfffgttgtreetrtgfgfggfgfdfdffgfgereerehhgvkjnnu
