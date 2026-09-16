db2a9ea76dc47608286a26218c445ec908a3ce54
Tarang Vaghasiya <tarangvaghasiya.cg@gmail.com>
2026-09-16 16:47:31 +0530

    gbgbgb

---
+++ 10.For_Loop/piramid.py
@@ -0,0 +1,4 @@
+rows = int(input("enter the rows:-"))
+
+for i in range(1,6):
+    for j in  range(1,6-i)
--- 10.For_Loop/te.py
+++ 10.For_Loop/te.py
@@ -5,11 +5,31 @@
 #         print("*",end="")
 #     print()
 
-for i in range(0,6):
-    for j in range(1,i+1):
-        print(j,end="")
-    print()
-for i in range(0,6):
-    for j in range(1,6-i):
-        print(j,end="")
+# for i in range(0,6):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()
+# for i in range(0,6):
+#     for j in range(1,6-i):
+#         print(j,end="")
+#     print()
+
+# for i in range(5):
+#     for j in range(i+1):
+#         print("*",end="")
+#     print()
+
+# n = int(input("enter your number"))
+# for i in range(1, n+1):
+#     for j in range(1,(n+1) - i):
+#         print(" ", end="")
+#     for k in range(1,i+1):
+#         print(k, end="")
+#     print()
+
+for i in range(1,6):
+    for j in range(0,i+1):
+        print(" ",end="")
+    for k in range(1,6-i):
+        print("*",end="")    
     print()
