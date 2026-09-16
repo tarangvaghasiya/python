40434ba2c7efae91399db9775391e681d6c222ea
Tarang Vaghasiya <tarangvaghasiya.cg@gmail.com>
2026-09-16 11:30:39 +0530

    dded

--- 10.For_Loop/as1.py
+++ 10.For_Loop/as1.py
@@ -222,5 +222,5 @@
 #         print(j,end="")
 #     print()
 
-vffhnhnhnhnhnhnhnhfdfdfdfdfwewewrgrgghghg
+vffhnhnhnhnhnhnhnhfdfdfdfdfwewewrgrgghghgdsd
 lklkhghghhhjhhjkjkjkhghhhfvcvuuyugfggfggggbgbg
